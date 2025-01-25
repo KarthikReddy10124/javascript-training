0e5110ddc369bc7226d21c4db008b5321f73b675
KarthikReddy10124 <karthikreddy10124@gmail.com>
2025-01-25 11:16:09 +0530

    commit

---
+++ day7/async.js
@@ -0,0 +1,82 @@
+// asynchronous function
+// a way of executing other functions to execute first 
+ // two methods 
+ // 1) setTimeout(callbackfunction , delay)
+//  2)setInterval(callbackfunction ,delay)
+//these methods are present inside a window object
+// window.setTimeout(()=>{
+//     console.log("im set time out and i print after  5 secs delay" )
+// },5000)
+
+
+
+// window.setInterval(()=>{
+//     console.log("im set interval and i log after every 3 secs");
+    
+// } ,2000)
+
+// function main(n,m){
+//     setTimeout(()=>{
+//         for(let i=n ; i<=m ;i++){
+//             console.log(i)
+//         }
+//     },2000)
+// } 
+// main(1,10)
+
+// function demo(){
+//     console.log("demo");
+
+// }
+// demo()
+
+
+// promise 
+//it is an object 
+// it is used to look after async function
+
+// 3 phases
+// 1) pending phase
+//2) resolve or fullfil 
+//3) reject phase
+
+// syntax
+// new promise ((resolve,reject)=>{
+   //    --async-function-code }).then(()=>{}).catch(()=>{})
+    // let online  = window.navigator.onLine
+    // new Promise((resolve,reject)=>{
+    //     setTimeout(()=>{
+    //         if(online === true){
+    //             return resolve()
+    //         }else{
+    //             return reject() 
+    //         }
+    //     },2000)
+    // }).then(()=>console.log("online😘😘😘")).catch(()=>console.log("offline😘😘😘"))
+
+function main (n,m){
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(isNaN(n)|| isNaN(m)){
+                return reject()
+            }
+            for(let i =n ; i<m ;i++){
+                console.log(i);
+                
+            }
+            return resolve()
+        },3000)
+    }).then(()=>{console.log("promise is resolved")})
+    .catch(()=>{console.log("promise is rejected")})
+}    
+main(1,10)
+function snumber(){
+    console.log("numbers are printing after 3 secs")
+}
+snumber()
+
+
+ //promise chaining
+ // can use multiple .then() methods
+ //
+
--- day9/async.js
+++ day9/async.js
@@ -1,20 +1,95 @@
-function main(m,n){
-    new Promise((resolve, reject) =>{
-        setTimeout(()=>{
-            if(isNaN(m) || isNaN(n))
-            {
-                return reject()
-            }
-            for(let i=m;i<=n;i++){
-                console.log(i);
-            }
-            return resolve()
-        },5000)
-    })
-    
-    .then(() => console.log("promise is resolved")
-    )
-    .catch(() => console.log("error occurs")
-    )
-    }
-    main(1, 10)
+// asynchroud function
+// -->making a way for other function to execut first.
+
+// two methods
+// 1. setTime out (callback function , delay-time)
+// 2. setInterval(cbf , delay-time)
+// present inside window object
+
+// window.setTimeout(() => {
+//     console.log("setTimeout");
+// },4500)
+// // 4500ms = 4.5sec
+// window.setInterval(() => {
+//     console.log("Interval");
+// },3500)
+
+// function main(m,n) {
+//     setTimeout(() => {
+//         for(let i=m; i<=n; i++)
+//         {
+//             console.log(i);
+//         }
+//     },5000)
+// }
+// main(1,10)
+// // main(1 , "10a")
+
+// function demo()
+// {
+//     console.log("demo");
+// }
+// demo()
+
+
+// promise
+// --> used to look after async function
+// --> a way to handle asynchronous operation
+// --> a way to handle the result of asynchronous operation
+// --> a way to handle the error of asynchronous operation
+
+//3-phase
+// 1. pending
+// 2. resolved / fulfill 
+// 3. rejected
+
+
+//                       |-------Pending(either it will be resolve or reject) 
+//                       |
+//                       |   
+//pending----------------|-------Resolve(async fun working properly)
+//                       |       .then(cbf)
+//                       |
+//                       |
+//                       |-------Reject (async fun not working prpoerly)
+//                               .catch(cbf)
+// syntax
+// new Promise((resolve, reject) => {
+//   --async-fun-code
+// })
+// .then(cbf)
+//  .catch(cbf)
+
+// let online = window.navigator.onLine
+
+// new Promise((resolve , reject) => {
+//     setTimeout(() => {
+//         if(online === true){
+//             return resolve()
+//         }else{
+//             return reject()
+//         }
+//     } , 2000)
+// })
+// .then(() => console.log("online ... 😍😎😗")
+// )
+// .catch(() => console.log("of-line ...🤷‍♂️😢🥲")
+// )
+
+
+function main(m,n) {
+    setTimeout(() => {
+        for(let i=m; i<=n; i++)
+        {
+            console.log(i);
+        }
+    },5000)
+}
+main(1,10)
+// main(1 , "10a")
+
+function demo()
+{
+    console.log("demo");
+}
+demo()
