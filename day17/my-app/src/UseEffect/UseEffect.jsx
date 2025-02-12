ff349f96cedf324f7822e4148428e4fb55a4079f
KarthikReddy10124 <karthikreddy10124@gmail.com>
2025-02-12 13:01:01 +0530

    useEffect and github fetch data

--- day17/my-app/src/UseEffect/UseEffect.jsx
+++ day17/my-app/src/UseEffect/UseEffect.jsx
@@ -40,25 +40,70 @@
 
 // export default UseEffect
 
-// dependenct with value 
+// // dependenct with value 
+// import React, { useEffect, useState } from 'react'
+
+// const UseEffect = () => {
+//     let [state , setState] = React.useState(0)
+//     let [incr , setIncr] = useState(0)
+
+//     useEffect(() => {
+//         setIncr(() => state * 2)
+//     } , [state])
+//   return (
+//     <div>
+//         {state}
+//         <br />
+//         <button onClick={() => setState((e) => e +1)}>dont click</button>
+//         <br />
+//         {incr}
+//     </div>
+//   )
+// }
+
+// export default UseEffect
+
+// fetch api using useEffect hook
+
 import React, { useEffect, useState } from 'react'
-
+import"./useEffect.css"
 const UseEffect = () => {
-    let [state , setState] = React.useState(0)
-    let [incr , setIncr] = useState(0)
+  let [state , setState] = useState([])
+    console.log(state);
+    
 
-    useEffect(() => {
-        setIncr(() => state * 2)
-    } , [state])
-  return (
-    <div>
-        {state}
-        <br />
-        <button onClick={() => setState((e) => e +1)}>dont click</button>
-        <br />
-        {incr}
-    </div>
-  )
-}
+    useEffect( () => {
+      window.fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((res) => setState(res))
+    } , [])
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>title</th>
+              <th>image</th>
+            </tr>
+          </thead>
+          {
+            state.map((res) => {
+              console.log(res);
+              return(
+                <>
+                <tr>
+                  <td>{res.id}</td>
+                  <td>{res.title}</td>
+                  <td> <img src={res.image} alt="" /> </td>
+                  </tr>
+                  </>
+              )
+          })
+        }
+        </table>
+      </div>
+    )
+  }
 
 export default UseEffect
